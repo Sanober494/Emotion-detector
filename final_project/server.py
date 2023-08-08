67556095448ff0fc56158af487c0d40fb8eceb06
Sanober494 <75628824+Sanober494@users.noreply.github.com>
2023-08-08 13:56:45 +0400

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -12,7 +12,11 @@ def emotion_detector_route():
     if request.method == 'POST':
         statement = request.form.get('statement')
         result = emotion_detector(statement)
-        return jsonify(result)
+        if result['dominant_emotion'] is None:
+            return "Invalid text! Please try again."
+        else:
+            return render_template('result.html', result=result)
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
