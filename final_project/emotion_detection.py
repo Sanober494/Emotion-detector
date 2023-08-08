8fef69a3b7932f3a1bcf6724bfbf8454ba2f119b
Sanober494 <75628824+Sanober494@users.noreply.github.com>
2023-08-08 13:38:42 +0400

    Update emotion_detection.py

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     # URL for the Emotion Detection service
@@ -22,11 +23,38 @@ def emotion_detector(text_to_analyze):
 
         # Check if the request was successful
         if response.status_code == 200:
-            # Parse the response JSON and extract the text attribute
+            # Parse the response JSON and extract emotion scores
             response_json = response.json()
-            detected_text = response_json.get('text')
-
-            return detected_text
+            emotions = response_json.get('emotions', {})
+            
+            # Extract required emotions and their scores
+            anger_score = emotions.get('anger', 0.0)
+            disgust_score = emotions.get('disgust', 0.0)
+            fear_score = emotions.get('fear', 0.0)
+            joy_score = emotions.get('joy', 0.0)
+            sadness_score = emotions.get('sadness', 0.0)
+            
+            # Find the dominant emotion
+            emotion_scores = {
+                'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score,
+            }
+            dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+            
+            # Construct the formatted output dictionary
+            output = {
+                'anger': anger_score,
+                'disgust': disgust_score,
+                'fear': fear_score,
+                'joy': joy_score,
+                'sadness': sadness_score,
+                'dominant_emotion': dominant_emotion
+            }
+            
+            return output
         else:
             print(f"Error: Emotion Detection request failed with status code {response.status_code}")
             return None
@@ -35,6 +63,6 @@ def emotion_detector(text_to_analyze):
         return None
 
 # Example usage
-text_to_analyze = "I feel so excited about my upcoming vacation!"
-detected_text = emotion_detector(text_to_analyze)
-print("Detected Emotion Text:", detected_text)
+text_to_analyze = "I am so happy I am doing this."
+result = emotion_detector(text_to_analyze)
+print(json.dumps(result, indent=4))  # Print formatted JSON output
